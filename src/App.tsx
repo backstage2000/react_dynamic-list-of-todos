12ad0cc90e76a487fec7b7a79446f4079afda7a5
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-09-01 09:40:50 +0300

    draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,42 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { fetchPostsFromTodos } from './services/fetchDataFromTodos';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [allPosts, setAllPosts] = useState<Todo[]>([]);
+  const [posts, setPosts] = useState<Todo[] | null>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  // const [loadingUsers, setLoadingUsers] = useState(true);
+  const [loadingPosts, setLoadingPosts] = useState(true);
+  const [updateAt, setUpdateAt] = useState<Date>(new Date());
+  const [selectedPosts, setSelectedPosts] = useState<Todo | null>(null);
+
+  function reload() {
+    setUpdateAt(new Date());
+    setError(null);
+  }
+
+  useEffect(() => {
+    setLoadingPosts(true);
+
+    let timerID: ReturnType<typeof setTimeout>;
+
+    fetchPostsFromTodos()
+      .then(data => {
+        setAllPosts(data);
+        setPosts(data);
+      })
+      .catch(() => setError('Request failed'))
+      .finally(() => {
+        timerID = setTimeout(() => setLoadingPosts(false), 300);
+      });
+
+    return () => clearTimeout(timerID);
+  }, [updateAt]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +51,46 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={allPosts}
+                setFilter={todos => setPosts(todos)}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loadingPosts && <Loader />}
+              {!loadingPosts && allPosts.length > 0 && (
+                <TodoList
+                  todos={posts}
+                  onSelect={setSelectedPosts}
+                  selectedPosts={selectedPosts}
+                />
+              )}
+              {!loadingPosts && !error && allPosts.length === 0 && (
+                <p className="title is-5">There are no users</p>
+              )}
+              {error && (
+                <p className="notification is-danger">
+                  {error}
+                  <button onClick={reload}>Reload</button>
+                </p>
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {loadingPosts ? (
+        <div className="modal is-active" data-cy="modal">
+          <div className="modal-background" />
+          <Loader />
+        </div>
+      ) : selectedPosts ? (
+        <TodoModal
+          todos={selectedPosts}
+          onClose={() => setSelectedPosts(null)}
+        />
+      ) : null}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,95 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  todos: Todo[] | null;
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+  setFilter: (todos: Todo[] | null) => void;
+};
+
+type FilterPosts = 'all' | 'active' | 'completed';
+
+export const TodoFilter: React.FC<Props> = ({ todos, setFilter }) => {
+  const [selectedStatus, setSelectedStatus] = useState<FilterPosts>('all');
+  const [query, setQuery] = useState('');
+
+  const handleCloseModal = () => {
+    setQuery('');
+  };
+
+  useEffect(() => {
+    if (!todos) {
+      setFilter(null);
+
+      return;
+    }
+
+    let filtered = todos;
+
+    if (selectedStatus === 'completed') {
+      filtered = filtered.filter(todo => todo.completed);
+    }
+
+    if (selectedStatus === 'active') {
+      filtered = filtered.filter(todo => !todo.completed);
+    }
+
+    if (query.trim() !== '') {
+      filtered = filtered.filter(todo =>
+        todo.title.toLowerCase().includes(query.toLowerCase()),
+      );
+    }
+
+    setFilter(filtered);
+  }, [todos, selectedStatus, query, setFilter]);
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            value={selectedStatus}
+            onChange={e => {
+              const value = e.target.value as FilterPosts;
+
+              setSelectedStatus(value);
+            }}
+            data-cy="statusSelect"
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          value={query}
+          onChange={e => {
+            setQuery(e.target.value);
+          }}
+          className="input"
+          placeholder="Search..."
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleCloseModal}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
